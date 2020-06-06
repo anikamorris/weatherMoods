613308fc1f345cda7b6d1bd04b4bf7ec7faccea2
anikamorris <anikamorris11@gmail.com>
2020-06-06 00:21:14 -0700

    updated persistence layer

--- weatherMoods/Models/PersistenceLayer.swift
+++ weatherMoods/Models/PersistenceLayer.swift
@@ -18,7 +18,6 @@ struct PersistenceLayer {
     private static let userDefaultsDayKeyValue = "DAY"
 
     init() {
-        self.loadMoods()
         self.loadMoodJournal()
         self.setDay()
     }
@@ -35,47 +34,47 @@ struct PersistenceLayer {
         userDefaults.synchronize()        
     }
     
-    private mutating func loadMoods() {
-       let userDefaults = UserDefaults.standard
-       guard
-           let moodData = userDefaults.data(forKey: PersistenceLayer.userDefaultsMoodsKeyValue),
-           let moods = try? JSONDecoder().decode([Mood].self, from: moodData) else {
-               return
-       }
-       self.moods = moods
-    }
-
-    @discardableResult
-    mutating func newMood(mood: String) -> Mood {
-        let newMood = Mood(mood: mood)
-        self.moods.insert(newMood, at: 0) // Prepend the mood to the array
-        self.saveMoods()
-        return newMood
-    }
-
-    private func saveMoods() {
-        guard let moodsData = try? JSONEncoder().encode(self.moods) else {
-            fatalError("could not encode list of moods")
-        }
-
-        let userDefaults = UserDefaults.standard
-        userDefaults.set(moodsData, forKey: PersistenceLayer.userDefaultsMoodsKeyValue)
-        userDefaults.synchronize()
-    }
-
-    mutating func setNeedsToReloadMoods() {
-        self.loadMoods()
-    }
-
-    mutating func updateMood(_ moodIndex: Int, newMood: String) {
-        self.moods[moodIndex].mood = newMood
-        self.saveMoods()
-    }
-
-    mutating func delete(_ moodIndex: Int) {
-        self.moods.remove(at: moodIndex)
-        self.saveMoods()
-    }
+//    private mutating func loadMoods() {
+//       let userDefaults = UserDefaults.standard
+//       guard
+//           let moodData = userDefaults.data(forKey: PersistenceLayer.userDefaultsMoodsKeyValue),
+//           let moods = try? JSONDecoder().decode([Mood].self, from: moodData) else {
+//               return
+//       }
+//       self.moods = moods
+//    }
+//
+//    @discardableResult
+//    mutating func newMood(mood: String) -> Mood {
+//        let newMood = Mood(mood: mood)
+//        self.moods.insert(newMood, at: 0) // Prepend the mood to the array
+//        self.saveMoods()
+//        return newMood
+//    }
+//
+//    private func saveMoods() {
+//        guard let moodsData = try? JSONEncoder().encode(self.moods) else {
+//            fatalError("could not encode list of moods")
+//        }
+//
+//        let userDefaults = UserDefaults.standard
+//        userDefaults.set(moodsData, forKey: PersistenceLayer.userDefaultsMoodsKeyValue)
+//        userDefaults.synchronize()
+//    }
+//
+//    mutating func setNeedsToReloadMoods() {
+//        self.loadMoods()
+//    }
+//
+//    mutating func updateMood(_ moodIndex: Int, newMood: String) {
+//        self.moods[moodIndex].mood = newMood
+//        self.saveMoods()
+//    }
+//
+//    mutating func delete(_ moodIndex: Int) {
+//        self.moods.remove(at: moodIndex)
+//        self.saveMoods()
+//    }
     
     private mutating func loadMoodJournal() {
        let userDefaults = UserDefaults.standard
